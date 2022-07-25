75ffabedccac911cef8c655a98293158759e326e
hwihun.cho <hwihun.cho@navercorp.com>
2022-07-26 00:16:50 +0900

    [22.7.2] list ctrl 작성 완료

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -19,7 +19,18 @@ export const write = async ctx => {
   }
 }
 
-export const list = async ctx => {}
+/**
+ * GET /api/posts
+ */
+export const list = async ctx => {
+  try {
+    const posts = await Post.find().exec()
+    ctx.body = posts
+  } catch (e) {
+    ctx.throw(500, e)
+  }
+}
+
 export const read = ctx => {}
 export const remove = ctx => {}
 export const update = ctx => {}
