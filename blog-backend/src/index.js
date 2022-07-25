ed50bf395746bbd621262626a432242f0209d7fe
hwihun.cho <hwihun.cho@navercorp.com>
2022-07-25 23:36:22 +0900

    [22.3.1] dotdotenv 적용

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const Koa = require('koa')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 
+const { PORT } = process.env
 const api = require('./api')
 const app = new Koa()
 const router = new Router()
@@ -9,6 +11,7 @@ const router = new Router()
 router.use('/api', api.routes())
 app.use(bodyParser())
 app.use(router.routes()).use(router.allowedMethods())
-app.listen(4000, () => {
-  console.log('Listening to port 4000')
+const port = PORT || 4000
+app.listen(port, () => {
+  console.log(`Listening to port %d`, port)
 })
