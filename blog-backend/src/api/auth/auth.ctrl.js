dcfe302d92c645a6ae1f2839b952023faf2cb825
hwihun.cho <hwihun.cho@navercorp.com>
2022-07-26 15:18:07 +0900

    [23.3] auth 라우터 구조 잡음

---
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -0,0 +1,15 @@
+export const register = async ctx => {
+  // 회원가입
+}
+
+export const login = async ctx => {
+  // 회원가입
+}
+
+export const check = async ctx => {
+  // 회원가입
+}
+
+export const logout = async ctx => {
+  // 회원가입
+}
