621a4047e871eaf23244950f724e05b684b8663e
hwihun.cho <hwihun.cho@navercorp.com>
2022-07-27 17:47:03 +0900

    [24.3.1] 버튼 props에 to가 있으면 StyledLink를 반환

--- blog-frontend/src/components/common/Button.js
+++ blog-frontend/src/components/common/Button.js
@@ -1,8 +1,8 @@
 import styled, { css } from 'styled-components'
-import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import palette from '../../lib/styles/palette'
 
-const StyledButton = styled.button`
+const buttonStyle = css`
   border: none;
   border-radius: 4px;
   font-size: 1rem;
@@ -36,17 +36,20 @@ const StyledButton = styled.button`
     `
   }
 `
-const Button = ({ to, history, ...rest }) => {
-  const navigate = useNavigate()
-  const onClick = e => {
-    if (to) {
-      navigate(to)
-    }
-    if (rest.onClick) {
-      rest.onClick(e)
-    }
-  }
-  return <StyledButton {...rest} onClick={onClick} />
+const StyledButton = styled.button`
+  ${buttonStyle}
+`
+
+const StyledLink = styled(Link)`
+  ${buttonStyle}
+`
+
+const Button = props => {
+  return props.to ? (
+    <StyledLink {...props} cyan={props.cyan ? 1 : 0} />
+  ) : (
+    <StyledButton {...props} />
+  )
 }
 
 export default Button
