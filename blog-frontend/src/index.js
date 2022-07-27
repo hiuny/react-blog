a52e2f5df11625905bf9058eb7df5cc6c77826d0
hwihun.cho <hwihun.cho@navercorp.com>
2022-07-27 15:47:24 +0900

    [24.2.4] 회원가입 구현

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
+import createSagaMiddleware from 'redux-saga'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import rootReducer from './modules/index';
+import rootReducer, { rootSaga } from './modules';
 
-const store = createStore(rootReducer, composeWithDevTools())
-
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+)
+sagaMiddleware.run(rootSaga)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- blog-frontend/src/lib/createRequestSaga.js
+++ blog-frontend/src/lib/createRequestSaga.js
@@ -1,6 +1,12 @@
 import { call, put } from 'redux-saga/effects'
 import { startLoading, finishLoading } from '../modules/loading'
 
+export const createRequestActionTypes = type => {
+  const SUCCESS = `${type}_SUCCESS`
+  const FAILURE = `${type}_FAILURE`
+  return [type, SUCCESS, FAILURE]
+}
+
 export default function createRequestSaga(type, request) {
   const SUCCESS = `${type}_SUCCESS`
   const FAILURE = `${type}_FAILURE`
@@ -11,7 +17,7 @@ export default function createRequestSaga(type, request) {
       const response = yield call(request, action.payload)
       yield put({
         type: SUCCESS,
-        palyload: response.data,
+        payload: response.data,
       })
     } catch (e) {
       yield put({
