d6ec34545b3ddbacbb856aaceee20989b883d5a2
hwihun.cho <hwihun.cho@navercorp.com>
2022-07-28 18:13:55 +0900

    [26.2.3][backend] sanitize-html 적용

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,8 +1,33 @@
-import Post from '../../models/post'
 import mongoose from 'mongoose'
+import sanitizeHtml from 'sanitize-html'
 import Joi from 'joi'
+import Post from '../../models/post'
+import sanitize from 'sanitize-html'
 
 const { ObjectId } = mongoose.Types
+const sanitizeOption = {
+  allowedTags: [
+    'h1',
+    'h2',
+    'b',
+    'i',
+    'u',
+    's',
+    'p',
+    'ul',
+    'ol',
+    'li',
+    'blockquote',
+    'a',
+    'img',
+  ],
+  allowedAttributes: {
+    a: ['href', 'name', 'target'],
+    img: ['src'],
+    li: ['class'],
+  },
+  allowedSchemes: ['data', 'http'],
+}
 
 export const getPostById = async (ctx, next) => {
   const { id } = ctx.params
@@ -56,13 +81,25 @@ export const write = async ctx => {
   }
 
   const { title, body, tags } = ctx.request.body
-  const post = new Post({ title, body, tags, user: ctx.state.user })
+  const post = new Post({
+    title,
+    body: sanitizeHtml(body, sanitizeOption),
+    tags,
+    user: ctx.state.user,
+  })
   try {
     await post.save()
     ctx.body = post
   } catch (e) {
     ctx.throw(500, e)
   }
+}
+
+const removeHtmlAndShorten = body => {
+  const filtered = sanitizeHtml(body, {
+    allowedTags: [],
+  })
+  return filtered.length < 200 ? filtered : `${filtered.slice(0, 200)}...`
 }
 
 /**
@@ -94,7 +131,7 @@ export const list = async ctx => {
     ctx.set('List-Page', Math.ceil(postCount / 10))
     ctx.body = posts.map(post => ({
       ...post,
-      body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+      body: removeHtmlAndShorten(post.body)
     }))
   } catch (e) {
     ctx.throw(500, e)
@@ -144,8 +181,13 @@ export const update = async ctx => {
     ctx.body = result.error
     return
   }
+  const nextData = { ...ctx.request.body } // 객체를 복사하고
+  // body 값이 주어졌으면 HTML 필터링
+  if (nextData.body) {
+    nextData.body = sanitizeHtml(nextData.body, sanitizeOption)
+  }
   try {
-    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+    const post = await Post.findByIdAndUpdate(id, nextData, {
       new: true, // 이 값을 설정하면 업데이트된 데이터를 반환합니다.
       // false일 때는 업데이트되기 전의 데이터를 반환합니다.
     }).exec()
